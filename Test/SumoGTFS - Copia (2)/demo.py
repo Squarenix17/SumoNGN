53a55bee555c342a9ac1e7ea1c8019f0a9fd0772
Gabriele <gabriele.ughetto2000@gmail.com>
2022-07-30 23:02:44 +0200

    Calculation of distance and verification of proximity between buses

--- Test/SumoGTFS - Copia (2)/demo.py
+++ Test/SumoGTFS - Copia (2)/demo.py
@@ -4,7 +4,8 @@ import string
 import sys
 import optparse
 import datetime
-from numpy import double
+import test
+from numpy import double, equal
 
 from pandas import options
 
@@ -35,15 +36,33 @@ def run():
         print(traci.vehicle.getIDCount())
 
         vehicle = traci.vehicle.getIDList()
+        for x in vehicle:
+                buss = x.split('_')
+                linee = buss[0]
+                depTimee = buss[1]
+                routee = buss[2]
+                print("BUS - LINE: " + str(linee[1:]) + " DEPART: " + str(depTimee[1:]) + " POSITION: " + str(traci.vehicle.getPosition(x)))
+
         for i in vehicle:
             bus = i.split('_')
             line = bus[0]
             depTime = bus[1]
             route = bus[2]
+            #print("BUS - LINE: " + str(line[1:]) + " DEPART: " + str(depTime[1:]) + " POSITION: " + str(traci.vehicle.getPosition(i)))
             
-            
-            print("BUS - LINE: " + str(line[1:]) + " DEPART: " + str(depTime[1:]) + " POSITION: " + str(traci.vehicle.getPosition(i)))
-
+            isNear = True
+            for j in vehicle:
+                distance = test.getDistance(traci.vehicle.getPosition(i), traci.vehicle.getPosition(j))
+                if i == j:
+                    continue
+                if distance <= 20:
+                    if isNear:
+                        print("The nearest buses to: Line" + str(line[1:]) + " dTime " + str(depTime[1:]))
+                        isNear = False
+                    otherBus = j.split('_')
+                    lineOB = otherBus[0]
+                    depTimeOB = otherBus[1]
+                    print("LINE: " + str(lineOB[1:]) + " DEPART: " + str(depTimeOB[1:]) + "is " + str(distance) + "[m] distant")
         #print(traci.vehicle.getIDList())
 
         #print(step)
--- Test/SumoGTFS - Copia (2)/test.py
+++ Test/SumoGTFS - Copia (2)/test.py
@@ -1,4 +1,21 @@
-text = "test"
+import math
+from turtle import distance
 
-with open('test.txt', 'w') as f:
-    f.write(text)
+from numpy import mat
+
+
+def getDistance(mainBusPos, otherBusPos):
+    
+    Xcoord = mainBusPos[0] - otherBusPos[0]
+    Ycoord = mainBusPos[1] - otherBusPos[1]
+
+
+    distance = round(math.sqrt(math.pow(Xcoord,2)+math.pow(Ycoord, 2)),2)
+
+    return distance
+
+
+coord1 = (3.50, 8.75)
+coord2 = (7.69, 7.853)
+
+print(getDistance(coord1, coord2))
